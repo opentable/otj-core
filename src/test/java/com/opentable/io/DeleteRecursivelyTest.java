ef98e9f9fdaa4380e103c2656504633749d9aeb8
Ben Hardy <bhardy@opentable.com>
2014-09-16 16:21:23 -0700

    bring parent up to date

--- src/test/java/com/opentable/io/DeleteRecursivelyTest.java
+++ src/test/java/com/opentable/io/DeleteRecursivelyTest.java
@@ -9,9 +9,7 @@ import java.nio.file.Files;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-import org.kitei.testing.lessio.AllowTmpDirAccess;
 
-@AllowTmpDirAccess
 public class DeleteRecursivelyTest
 {
     @Rule
